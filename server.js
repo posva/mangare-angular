4e33cc56945142d47cd347c0df49aad2918d2d07
Eduardo San Martin Morote <posva13@gmail.com>
2015-05-08 19:32:51 +0200

    Fix port for heroku

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/provide', function(req, res) {
 
 app.use(express.static('public'));
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
 
   var host = server.address().address;
   var port = server.address().port;
