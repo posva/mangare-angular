b6178919207c0c46a14d0f36b65785679998c3a6
Eduardo San Martin Morote <posva13@gmail.com>
2015-05-08 21:07:03 +0200

    Update 2015-05-08T19:07:01.514Z

--- app/module.js
+++
@@ -1,8 +0,0 @@
-angular.module('Mangare', ['ngMaterial', 'ngMdIcons']).
-constant('Fuse', window.Fuse).
-config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('docs-dark', 'default')
-    .primaryPalette('yellow')
-    .accentPalette('light-green')
-    .dark();
-});
